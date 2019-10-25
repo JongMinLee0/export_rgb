5c6ea7a1aed2babcfe633c976a94e98a6dd66d33
JongminLee <jongminlee0218@gmail.com>
2019-10-25 15:25:00 +0900

    fix : tomcat config
    using external tomcat server

--- src/main/java/com/jong/export/ExportApplication.java
+++ src/main/java/com/jong/export/ExportApplication.java
@@ -2,12 +2,20 @@ package com.jong.export;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExportApplication {
+public class ExportApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ExportApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExportApplication.class, args);
     }
 
+
 }
+
